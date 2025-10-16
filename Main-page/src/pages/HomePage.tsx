03f72e52dc0a57b88e74ac9ae3a28124d1a32281
honggochu <hcw1592@gmail.com>
2025-10-16 14:42:39 +0900

    메인페이지 카테고리 표시 수정

--- Main-page/src/pages/HomePage.tsx
+++ Main-page/src/pages/HomePage.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import HeroBanner from '../components/HeroBanner'
-import CategorySection from '../components/CategorySection'
 import WeeklyFocus from '../components/WeeklyFocus'
 
 const HomePage: React.FC = () => {
   return (
     <div className="w-full">
       <HeroBanner />
-      <CategorySection />
       <WeeklyFocus />
     </div>
   )
